4cafab9c6b25cd6d74cd782523ba4b459b320947
Enora GICQUEL <egicquel@mailjet.com>
2020-05-01 16:12:57 +0200

    Camera behavior

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
+    [SerializeField]
+    private float cameraOffset;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float interpolant;
+    
+    private Transform characterTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterTransform = GameObject.FindObjectOfType<PlayableCharacter>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 destinationPosition = this.transform.position;
+        destinationPosition.y = characterTransform.position.y + cameraOffset;
+        this.transform.position = Vector3.Lerp(this.transform.position, destinationPosition, interpolant);
     }
 }
