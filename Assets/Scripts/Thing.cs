f4bc7e6d91c896a00c6f76e80e20cf8b02a5d7a9
Eric <lamaison.eric@gmail.com>
2020-05-02 13:18:50 +0200

    range objects

---
+++ Assets/Scripts/Thing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thing : Object
+{
+
+    private int weight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsInRange())
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                player.PickupClothes(weight);
+            }
+        }
+    }
+
+    public void SetWeight(int w)
+    {
+        weight = w;
+    }
+
+    public int GetWeight()
+    {
+        return weight;
+    }
+}
