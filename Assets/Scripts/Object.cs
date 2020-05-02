63ad1bc6b64c8d2c1fed6d55e5f5093d2c58e0fc
Eric <lamaison.eric@gmail.com>
2020-05-02 17:08:25 +0200

    bleuh suite du truc

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    [SerializeField] protected PlayableCharacter player;
+    [SerializeField] protected float maxDistance;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    protected void Initialize()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    protected bool IsInRange()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist <= maxDistance)
+        {
+
+            if (!animator.GetBool("shining"))
+            animator.SetBool("shining", true);
+            return true;
+        }
+        else
+        {
+            if(animator.GetBool("shining"))
+            {
+                animator.SetBool("shining", false);
+            }
+            return false;
+        }
+    }
+}
